bd8e8481b56b2fb048fb3382ae77a1ca6ddc2740
sri <sane305823@results-cx.com>
2023-07-25 23:31:34 +0530

    second commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages,setup
+
+
+
+setup(
+
+name="ML_PROJECT_ETOE",
+version="0.0.1",
+author="srikanth",
+author_email="srikanthkumar.a@gmail.com",
+packages=find_packages(),
+install_requires=['pandas','numpy','seaborn']
+
+
+
+
+
+
+)
